4027acc7e0cf98d292164db253bef79e65bc06f3
vitalkumar123 <30402470+vitalkumar123@users.noreply.github.com>
2017-07-24 13:25:37 +0530

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to GIT HUB");
+            Console.WriteLine("Updated from GIT HUb");
+            
         }
     }
 }
