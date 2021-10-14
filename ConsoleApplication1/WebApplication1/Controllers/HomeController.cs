bf6efb7f4104f615afaf36d135228afbdbc8c81b
Vital Kumar <vital.nagavarapu@outlook.com>
2021-10-14 19:02:40 +0530

    checkin 2 to mybranch

--- ConsoleApplication1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApplication1/WebApplication1/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             ViewBag.Test = "sss";
+            ViewBag.QA = "QA";
             return View();
         }
 
